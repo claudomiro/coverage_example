2bdc287a05b35e320a44bc0b092c6008dcac532a
claudomiro <claudomiro@gmail.com>
2020-03-20 05:15:30 -0300

    (Minor) renomeando variavel

--- src/test/java/org/claudomiro/clean_code/world_cup/MainTest.java
+++ src/test/java/org/claudomiro/clean_code/world_cup/MainTest.java
@@ -8,55 +8,54 @@ import java.util.List;
 import static org.claudomiro.clean_code.world_cup.Group.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 import static org.claudomiro.clean_code.world_cup.Country.*;
 
 
 public class MainTest {
 
-    private Main main;
+    private Main wc2018;
 
     @BeforeEach
     void setUp() {
-        main = new Main();
+        wc2018 = new Main();
     }
 
     @Test
     public void whenValidCountryNameReturnPositionOfElination()
     {
 //        assertThat(main.position("France"), emptyOptional());
-        assertThat(Phase.QUARTER_FINALS, equalTo(main.elimination("Brazil")));
-        assertThat(Phase.ROUND_OF_16, equalTo(main.elimination("Germany")));
+        assertThat(Phase.QUARTER_FINALS, equalTo(wc2018.elimination("Brazil")));
+        assertThat(Phase.ROUND_OF_16, equalTo(wc2018.elimination("Germany")));
     }
 
     @Test
     void whenInvalidCountryThrowsException()
     {
         Assertions.assertThrows(IllegalArgumentException.class, () ->
-            main.elimination("Country that not exists"));
+            wc2018.elimination("Country that not exists"));
         Assertions.assertThrows(IllegalArgumentException.class, () ->
-            main.elimination("China"));
+            wc2018.elimination("China"));
 
     }
 
     @Test
     public void whenValidGroupReturnCountiresListOrderedByName()
     {
-        assertThat(main.byGroup(C), contains(AU, DK, FR, PE));
-        assertThat(main.byGroup(E), contains(BR, CR, RS, CH));
+        assertThat(wc2018.byGroup(C), contains(AU, DK, FR, PE));
+        assertThat(wc2018.byGroup(E), contains(BR, CR, RS, CH));
     }
 
     @Test
     public void whenEliinationPahseReturnCoutryList()
     {
-        assertThat(main.byEliminationPhase(Phase.FINALS), contains(BE, HR, GB_ENG));
+        assertThat(wc2018.byEliminationPhase(Phase.FINALS), contains(BE, HR, GB_ENG));
     }
 
     @Test
     public void returnAllCountries()
     {
-        final List<Country> all = main.allOrderedByCountryName();
+        final List<Country> all = wc2018.allOrderedByCountryName();
         assertThat(all, hasSize(32));
         assertThat(all.get(0), equalTo(AR));
         assertThat(all.get(13), equalTo(IR));
@@ -67,9 +66,9 @@ public class MainTest {
     @Test
     public void returnAllCoutryNames()
     {
-        List<String> allNames = main.allNames();
+        List<String> allNames = wc2018.allNames();
         assertThat(allNames, hasSize(32));
-        assertThat(main.allNames().get(0), equalTo("Argentina"));
+        assertThat(wc2018.allNames().get(0), equalTo("Argentina"));
         assertThat(allNames.get(13), equalTo("Iran"));
         assertThat(allNames.get(31), equalTo("Uruguay"));
     }
