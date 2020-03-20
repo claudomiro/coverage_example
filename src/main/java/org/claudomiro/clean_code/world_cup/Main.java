51bcb3ad64380db670c94a026be985f0b95c1982
claudomiro <claudomiro@gmail.com>
2020-03-20 03:41:38 -0300

    Retornando lista de todos

--- src/main/java/org/claudomiro/clean_code/world_cup/Main.java
+++ src/main/java/org/claudomiro/clean_code/world_cup/Main.java
@@ -5,6 +5,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.claudomiro.clean_code.world_cup.Country.*;
 import static org.claudomiro.clean_code.world_cup.Country.CH;
@@ -20,17 +21,33 @@ public class Main
                 .orElseThrow(() -> new IllegalArgumentException(constructErrorMessage(countryName)));
     }
 
-    private String constructErrorMessage(String countryName) {
-        return String.format("Country '%s' " +
-                    "is not a valid Country for the World Cup 2018!", countryName);
+    public List<Country> byGroup(final Group group)
+    {
+        return getCountryStream()
+                .filter(country -> group.equals(country.getGroup()))
+                .collect(Collectors.toList());
     }
 
-    public List<Country> byGroup(final Group group) {
-
-        return Arrays.stream(values())
-                .filter(country -> group.equals(country.getGroup()))
-                .sorted((c1, c2) -> new CountryNameComparator().compare(c1, c2))
+    public List<Country> allOrderedByCountryName()
+    {
+        return getCountryStream()
                 .collect(Collectors.toList());
+    }
+
+    public List<String> allNames() {
+        return getCountryStream()
+                .map(c -> c.getName())
+                .collect(Collectors.toList());
+    }
+
+    private String constructErrorMessage(String countryName) {
+        return String.format("Country '%s' " +
+                "is not a valid Country for the World Cup 2018!", countryName);
+    }
+
+    private Stream<Country> getCountryStream() {
+        return Arrays.stream(values())
+                .sorted((c1, c2) -> new CountryNameComparator().compare(c1, c2));
     }
 
 }
--- src/test/java/org/claudomiro/clean_code/world_cup/MainTest.java
+++ src/test/java/org/claudomiro/clean_code/world_cup/MainTest.java
@@ -1,19 +1,17 @@
 package org.claudomiro.clean_code.world_cup;
 
 import org.junit.jupiter.api.*;
-import org.omg.SendingContext.RunTime;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 
 import static org.claudomiro.clean_code.world_cup.Group.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import static org.claudomiro.clean_code.world_cup.Country.*;
+
 
 public class MainTest {
 
@@ -47,6 +45,27 @@ public class MainTest {
     {
         assertThat(main.byGroup(C), contains(AU, DK, FR, PE));
         assertThat(main.byGroup(E), contains(BR, CR, RS, CH));
+    }
+
+    @Test
+    public void returnAllCountries()
+    {
+        final List<Country> all = main.allOrderedByCountryName();
+        assertThat(all, hasSize(32));
+        assertThat(all.get(0), equalTo(AR));
+        assertThat(all.get(13), equalTo(IR));
+        assertThat(all.get(31), equalTo(UY));
+
+    }
+
+    @Test
+    public void returnAllCoutryNames()
+    {
+        List<String> allNames = main.allNames();
+        assertThat(allNames, hasSize(32));
+        assertThat(main.allNames().get(0), equalTo("Argentina"));
+        assertThat(allNames.get(13), equalTo("Iran"));
+        assertThat(allNames.get(31), equalTo("Uruguay"));
     }
 
 }
